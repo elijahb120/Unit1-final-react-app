c6384f79f934fdb2a40ea52423abeff79cd89205
Eli Basler <elijahb3851@gmail.com>
2025-07-15 19:17:37 -0500

    added files from my test  app

---
+++ unit1-final-react-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 id="title">Camping planner</h1>
+      <nav id="nav-bar">
+         <button class="nav-button">Home</button>
+         <button class="nav-button">About</button>
+         <button class="nav-button">Plan</button>
+         <button class="nav-button">MyTrips</button>
+         
+      </nav>
+    </header>
+  );
+}
---
+++ unit1-final-react-app/src/components/HomePage.jsx
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+     <>
+     <div id="top-box">
+         <img></img>
+         <p></p>
+     </div>
+     <div id="middle-box">
+      <p></p>
+      <img></img>
+     </div>
+     <div id="bottom-box">
+        <img></img>
+         <p></p>
+     </div>
+
+  </>
+  );
+}
